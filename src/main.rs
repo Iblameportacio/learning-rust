53f215530ae703407e91751dbaa2d3afd2685e14
Iblameportacio <gm.portacio@gmail.com>
2026-04-13 02:47:55 -0500

    otro ejercicio lmao

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,8 @@
-//En lugar de variables sueltas, vamos a agrupar datos.
-//Tarea: Crea una struct llamada Laptop con los campos: modelo (String), ram (u32) y precio (f32).
-//Lógica: Instancia una laptop (ej. una T470), cámbiale el precio (necesitarás let mut) e imprímela usando {:?}.
-//Tip: Recuerda añadir #[derive(Debug)] arriba de la struct para poder imprimirla.
-#[derive(Debug)]
-struct Laptop {
-    modelo: String,
-    ram: u32,
-    precio: f32,
-}
-
+///Tarea: Crea un vec!["ls", "cd", "sudo", "pacman"].
+//Lógica: Usa .iter(), .map() y el método .to_uppercase() para convertir todos los comandos a mayúsculas.
+//Output esperado: ["LS", "CD", "SUDO", "PACMAN"] guardado en un nuevo Vec<String>.
 fn main() {
-    let mut t470 = Laptop {
-        modelo: String::from("T470"),
-        ram: 16,
-        precio: 600.0_f32,
-    };
-    t470.precio = 550.0;
-    println!("{}", t470.modelo);
-    println!("{}", t470.ram);
-    println!("{}", t470.precio);
+    let comandos = vec!["ls", "cd", "sudo", "pacman"];
+    let comandos_mayusculas: Vec<String> = comandos.iter().map(|c| c.to_uppercase()).collect();
+    println!("{:?}", comandos_mayusculas);
 }
