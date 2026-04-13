fcc2a77290b01b85cdbe28eb24d8d45cb9d1de0c
Iblameportacio <gm.portacio@gmail.com>
2026-04-13 00:39:19 -0500

    feat: ejercicios de notas

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
-//Ya sabemos que tu primo mide 2.16m. Vamos a hacer una lista de alturas y filtrar quiénes son "Gigantes".
-//Tarea: Crea un vec![1.70, 1.90, 2.16, 1.65, 2.05].
-//Lógica: Usa .iter() y .filter() para obtener solo las alturas mayores a 2.00m.
-//Output esperado: "Los gigantes son: [2.16, 2.05]"
+//Vamos a convertir notas numéricas a conceptos usando el match que tanto nos sirve.
+//Tarea: Crea una variable nota (f32).
+//Lógica: Usa un match (puedes convertir la nota a entero con as i32 si quieres usar rangos como 0..=2).
+//3.0 a 3.9 -> "Pasaste raspando"
+//4.0 a 5.0 -> "bien ahi bro"
+//Menos de 3.0 -> "F por ti, broski"
+//Output: El mensaje correspondiente.
 fn main() {
-    let alturas: Vec<f32> = vec![1.70, 1.90, 2.16, 1.65, 2.05];
-    let gigantes = alturas.iter().filter(|&h| h > &2.00).collect::<Vec<_>>();
-    println!("Los gigantes son: {:?}", gigantes);
+    let nota: f32 = 4.5;
+    match nota {
+        3.0..=3.9 => println!("Pasaste raspando"),
+        4.0..=5.0 => println!("bien ahi bro"),
+        _ => println!("F por ti, broski"),
+    }
 }
