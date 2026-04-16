fea8783fb0e90d8406ec32a2eb45a04791cf0c92
Iblameportacio <gm.portacio@gmail.com>
2026-04-16 16:38:47 -0500

    ejercicio tabla del 7

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,12 @@
-//fizzbuzz del 1 al 50 broski
+//Objetivo: Imprimir la tabla del 7, desde $7 \times 1$ hasta $7 \times 10$.Lo que usas:
+// Un bucle for que vaya del 1 al 10 y una multiplicación simple.
+// Resultado esperado: Deberías ver en consola: 7 x 1 = 7, 7 x 2 = 14, etc.
 fn main() {
-    for i in 1..=50 {
-        if i % 3 == 0 && i % 5 == 0 {
-            println!("FizzBuzz");
-        } else if i % 3 == 0 {
-            println!("Fizz");
-        } else if i % 5 == 0 {
-            println!("Buzz");
-        } else {
-            println!("{}", i);
-        }
+    let tabla_del = 7; // Tu variable fija
+
+    for i in 1..=10 {
+        // El for crea la variable 'i' que va cambiando
+        let resultado = tabla_del * i; // Aquí sacas el resultado en cada vuelta
+        println!("{} x {} = {}", tabla_del, i, resultado);
     }
 }
