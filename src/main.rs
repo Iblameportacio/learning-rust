c02d1f1259b8648b3a3a23d44a92b23d0e9a720e
Iblameportacio <gm.portacio@gmail.com>
2026-04-16 17:44:12 -0500

    bucle 1 al 10

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
-//Objetivo: Imprimir la tabla del 7, desde $7 \times 1$ hasta $7 \times 10$.Lo que usas:
-// Un bucle for que vaya del 1 al 10 y una multiplicación simple.
-// Resultado esperado: Deberías ver en consola: 7 x 1 = 7, 7 x 2 = 14, etc.
+//crea un bucl con numeros del 1 al 10, si es mayor a 10 es grande sino es pequeño
 fn main() {
-    let tabla_del = 7; // Tu variable fija
-
-    for i in 1..=10 {
-        // El for crea la variable 'i' que va cambiando
-        let resultado = tabla_del * i; // Aquí sacas el resultado en cada vuelta
-        println!("{} x {} = {}", tabla_del, i, resultado);
+    for i in 1..=15 {
+        if i > 10 {
+            println!("el numero {} es grande", i)
+        } else {
+            println!("el numero {} es pequeño", i)
+        }
     }
 }
