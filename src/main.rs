11c2f8159fe14bdce729690656ca6b337bb063a7
Iblameportacio <gm.portacio@gmail.com>
2026-04-17 18:51:03 -0500

    factorial de 6

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,8 @@
-//Objetivo: Haz un bucle del 1 al 20.
-//Instrucción: Si el número es múltiplo de 3, imprime: "[número] es múltiplo de 3". Si no lo es, no imprimas nada.
-//Pista: Recuerda el operador residuo %. Si i % 3 == 0, es múltiplo.
-//Lo que practicas: Filtrado selectivo dentro de un bucle.
+//calula el prdocuto de 6!
 fn main() {
-    for i in 1..=20 {
-        if i % 3 == 0 {
-            println!("{} es multiplo de 3", i)
-        } else {
-            //no hacer nada lmao
-        }
+    let mut producto: i16 = 1;
+    for i in (1..=6).rev() {
+        producto *= i;
     }
+    println!("el resultado es {}", producto)
 }
