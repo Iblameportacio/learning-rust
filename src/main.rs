8970d53f230867b8114177ab0eb861c10641bdb2
Iblameportacio <gm.portacio@gmail.com>
2026-04-17 18:16:14 -0500

    ejercicio multiplos de 3

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,13 @@
-//Afuera del bucle: Una variable mut total = 0;.
-//Dentro del bucle:
-//Un rango del 1 al 5.
-//Calculas el cuadrado (i * i).
-//Se lo sumas al total.
-//Afuera del bucle: Imprimes el total final.
+//Objetivo: Haz un bucle del 1 al 20.
+//Instrucción: Si el número es múltiplo de 3, imprime: "[número] es múltiplo de 3". Si no lo es, no imprimas nada.
+//Pista: Recuerda el operador residuo %. Si i % 3 == 0, es múltiplo.
+//Lo que practicas: Filtrado selectivo dentro de un bucle.
 fn main() {
-    let mut total: i32 = 0;
-    for i in 1..=5 {
-        let cuadrado: i32 = i * i;
-        total = total + cuadrado;
+    for i in 1..=20 {
+        if i % 3 == 0 {
+            println!("{} es multiplo de 3", i)
+        } else {
+            //no hacer nada lmao
+        }
     }
-
-    println!("el total es {:?}", total)
 }
