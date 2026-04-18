81a79ac0cad0b61e0982ee96445adaa591229a4c
Iblameportacio <gm.portacio@gmail.com>
2026-04-17 21:35:33 -0500

    divisores del 24

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
-//hacer una escalera de asteriscos
+//Objetivo: Dado el número 24, cuenta cuántos divisores exactos tiene
+// (números que lo dividen y dejan residuo 0).
+//Instrucción: 1.  Crea una variable fija let numero = 24;.
+//2.  Crea un acumulador mut contador = 0;.
+//3.  Haz un bucle del 1 al 24.
+//4.  Si numero % i == 0, súmale 1 al contador.
+//Resultado esperado: "El número 24 tiene [X] divisores"
 fn main() {
-    for i in 1..=5 {
-        let resultado: String = "*".repeat(i);
-        println!("{}", resultado)
+    let numero: u8 = 24;
+    let mut contador = 0;
+    for i in 1..=24 {
+        if numero % i == 0 {
+            contador = contador + 1
+        }
     }
+    println!("el numero 24 tiene {} divisores", contador)
 }
