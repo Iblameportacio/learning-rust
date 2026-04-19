1187e0ca08582c19ffdd68d8bc12b70ec3d4aa94
Iblameportacio <gm.portacio@gmail.com>
2026-04-19 06:13:35 -0500

    las portencias de dos del 1 al 10

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
-//El 7 de la suerte: Imprime los números del 1 al 50, pero si es múltiplo de 7, imprime "¡SUERTE!".
+//Potencias de 2: Imprime las potencias de 2 ($2^1, 2^2, ...$) hasta llegar a $2^{10}$.
 fn main() {
-    for i in 1..=50 {
-        println!("{}", i);
-        if i % 7 == 0 {
-            println!("¡SUERTE!");
-        }
+    let dos: i32 = 2;
+    for i in 1..=10 {
+        println!("{} ^ {} = {}", dos, i, 2_i32.pow(i));
     }
 }
+//no bro me siento como si hubiera tomado nzt lmaoo
