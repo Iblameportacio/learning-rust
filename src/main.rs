7fe2172b253d0f935e6bca86e26d275b1d8551bc
Iblameportacio <gm.portacio@gmail.com>
2026-04-19 05:55:57 -0500

    numeros sumados del 1 al 50

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,8 @@
-//Multiplicador Variable: Pide un número y muestra su tabla del 1 al 12.
-use std::io;
-
+//Suma de Pares: Suma todos los números pares del 1 al 50.
 fn main() {
-    println!("escoje un numero para mostrar su tabla de multi\n");
-    let mut entrada = String::new();
-
-    io::stdin().read_line(&mut entrada).unwrap();
-
-    let numero: i32 = entrada.trim().parse().unwrap();
-
-    println!("la tabla de multiplicar es:\n");
-
-    for i in 1..=12 {
-        println!("{} x {} = {}", numero, i, numero * i);
+    let mut inicio = 0;
+    for i in 1..=50 {
+        inicio = inicio + i;
     }
+    println!("{}", inicio)
 }
