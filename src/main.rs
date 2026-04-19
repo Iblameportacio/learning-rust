1f6dce57698a9cfcbb47e678a23ff5ad6ab77205
Iblameportacio <gm.portacio@gmail.com>
2026-04-19 05:46:20 -0500

    tabla de multiplicar por input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+//Multiplicador Variable: Pide un número y muestra su tabla del 1 al 12.
+use std::io;
+
 fn main() {
-    println!("lo siguiente seran ejericcios en mi pomodoro de 5 horas");
+    println!("escoje un numero para mostrar su tabla de multi\n");
+    let mut entrada = String::new();
+
+    io::stdin().read_line(&mut entrada).unwrap();
+
+    let numero: i32 = entrada.trim().parse().unwrap();
+
+    println!("la tabla de multiplicar es:\n");
+
+    for i in 1..=12 {
+        println!("{} x {} = {}", numero, i, numero * i);
+    }
 }
