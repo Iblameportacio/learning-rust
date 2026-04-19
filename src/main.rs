a87818a84ea3f96828332ba02aa133a704592298
Iblameportacio <gm.portacio@gmail.com>
2026-04-19 06:01:27 -0500

    impares del 1 al 100

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
-//Suma de Pares: Suma todos los números pares del 1 al 50.
+//Contador de Impares: Cuenta cuántos números impares hay entre 1 y 100.
 fn main() {
     let mut inicio = 0;
-    for i in 1..=50 {
-        inicio = inicio + i;
+    for i in 1..=100 {
+        if i % 2 != 0 {
+            inicio = inicio + 1;
+        }
     }
-    println!("{}", inicio)
+    println!(
+        "el numero de pares de los numeros del 1 al 100 son\n {}",
+        inicio
+    )
 }
+//ya empiezo a notar que mi logica de prograacion ha mejorado mucho
