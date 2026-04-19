8ce10f6985de7f268a2c55265e81e25dc26da88c
Iblameportacio <gm.portacio@gmail.com>
2026-04-19 08:14:09 -0500

    funcion es par

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
-//Conversor de C/F: Función que pase de grados Celsius a Fahrenheit.
+//Es Par?: Función que devuelva bool si un número es par.
 fn main() {
-    let gradoc: f32 = 0.0;
-    let resultado = fahrenheit(gradoc);
-    println!("los grados {} a fahrenheit son {}", gradoc, resultado);
+    let cinco: f32 = 5.0;
+    let resultado = espar(cinco);
+    println!("este numero {} es par?{}", cinco, resultado);
 }
-fn fahrenheit(n: f32) -> f32 {
-    let n = (n * 1.8) + 32.0;
-    n
+fn espar(n: f32) -> bool {
+    if n % 2.0 == 0.0 {
+        return true;
+    } else {
+        return false;
+    }
 }
