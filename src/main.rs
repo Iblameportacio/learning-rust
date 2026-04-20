73dd9a0ccb898f4ff5819d6adaff0976fb297cbf
Iblameportacio <gm.portacio@gmail.com>
2026-04-19 23:22:17 -0500

    el mayor de todos en una tupla

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
-//Es Par?: Función que devuelva bool si un número es par.
+//Máximo de Tres: Función que reciba 3 números y devuelva el mayor de ellos.
 fn main() {
-    let cinco: f32 = 5.0;
-    let resultado = espar(cinco);
-    println!("este numero {} es par?\n{}", cinco, resultado);
+    let numeros: (i32, i32, i32) = (1, 432, 873);
+    let resultado = elmayor(numeros);
+    println!("en {:?} el mayor es {:?}", numeros, resultado);
 }
-fn espar(n: f32) -> bool {
-    if n % 2.0 == 0.0 {
-        return true;
+fn elmayor(nums: (i32, i32, i32)) -> i32 {
+    if nums.0 > nums.1 && nums.0 > nums.2 {
+        return nums.0;
+    } else if nums.1 > nums.0 && nums.1 > nums.2 {
+        return nums.1;
     } else {
-        return false;
+        return nums.2;
     }
 }
