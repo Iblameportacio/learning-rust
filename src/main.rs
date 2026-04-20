f17356a0a84b9b172a44d2505270d64db373b4c3
Iblameportacio <gm.portacio@gmail.com>
2026-04-20 16:51:57 -0500

    promedio de notas para ver si pase, se que dice f3 pero lo hize f64 para mayor precision en los decimales lmao

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
-//Calculadora de IVA: Función que reciba un precio y le sume el 19% (IVA de Colombia).
+//Promedio de Notas: Función que reciba 3 notas ($f32$) y diga si aprueba (mayor a 3.0).
 fn main() {
-    let compra: f64 = 40.000;
-    let total = iva(compra);
-    println!(
-        "la compra de {:?} termina siendo {:?} con un 19% de iva",
-        compra, total
-    );
+    let nota1: f64 = 4.0;
+    let nota2: f64 = 1.0;
+    let nota3: f64 = 3.0;
+    let notas: (f64, f64, f64) = (nota1, nota2, nota3);
+    let solucion: f64 = promedio(notas);
+    println!(" el promedio de las notas {:?} es {:?}", notas, solucion);
 }
-fn iva(n: f64) -> f64 {
-    let n: f64 = (n * 0.19) + n;
-    n
+fn promedio(nums: (f64, f64, f64)) -> f64 {
+    let nums = (nums.0 + nums.1 + nums.2) / 3.0;
+    nums
 }
