c0d29bdf8772e1e20b19c4a7e4abc1cbd26f5b6d
Iblameportacio <gm.portacio@gmail.com>
2026-04-21 08:05:02 -0500

    suma en un vector

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
-//Suma de Vector: Crea un vector con 5 números y suma todos sus elementos.
+use std::vec;
+
+//Buscador: Crea un vector y una función que diga si el número "10" está dentro.
 fn main() {
-    let v = vec![1, 2, 3, 4, 5];
-    let suma = v[0] + v[1] + v[2] + v[3] + v[4];
-    println!(
-        "{:?} + {:?} + {:?} + {:?} + {:?} = {:?}",
-        v[0], v[1], v[2], v[3], v[4], suma
-    )
+    let v: Vec<i32> = vec![1, 2, 3, 4, 5, 6, 8, 9, 10, 11];
+    let resultado: bool = buscar(&v);
+    println!("esta 10 en el vector {:?} ? {:?}", v, resultado);
+}
+fn buscar(n: &Vec<i32>) -> bool {
+    n.contains(&10)
 }
