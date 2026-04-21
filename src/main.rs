60f9473542b30adfdac8900354b1a667bccc818a
Iblameportacio <gm.portacio@gmail.com>
2026-04-21 08:42:24 -0500

    vectores en negativo y positivo

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use std::vec;
-//Inversor Manual: Crea un vector y pásalo a otro vector pero con los elementos al revés.
+//Filtro de Positivos: Dado un vector con números negativos y positivos,
+// crea uno nuevo solo con los positivos.
 fn main() {
-    let v: Vec<i32> = vec![1, 2, 3, 4, 5, 6, 8, 9, 10, 11];
-    let resultado: Vec<i32> = v.iter().rev().cloned().collect();
-    println!("el vector {:?} invertido es {:?}", v, resultado);
+    let v: Vec<i32> = vec![1, 2, 3, 4, -5, 6, 8, -9];
+    let ve: Vec<i32> = vec![1, 2, 3, 4, 5, 6, 8, 9];
+    println!("el vector v es {:?} y el vector ve es {:?}", v, ve);
 }
