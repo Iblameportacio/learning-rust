af4df38ec691429044e9c36b87d82be97fc3e85d
Iblameportacio <gm.portacio@gmail.com>
2026-04-21 08:30:27 -0500

    inertir vectores

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
 use std::vec;
-
-//Buscador: Crea un vector y una función que diga si el número "10" está dentro.
+//Inversor Manual: Crea un vector y pásalo a otro vector pero con los elementos al revés.
 fn main() {
     let v: Vec<i32> = vec![1, 2, 3, 4, 5, 6, 8, 9, 10, 11];
-    let resultado: bool = buscar(&v);
-    println!("esta 10 en el vector {:?} ? {:?}", v, resultado);
-}
-fn buscar(n: &Vec<i32>) -> bool {
-    n.contains(&10)
+    let resultado: Vec<i32> = v.iter().rev().cloned().collect();
+    println!("el vector {:?} invertido es {:?}", v, resultado);
 }
