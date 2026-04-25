a81d433964510610b29c04956b22bb7159846fcd
Iblameportacio <gm.portacio@gmail.com>
2026-04-24 21:03:02 -0500

    suma de varios numeros en uno solo

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
-//Password Checker: Función que reciba un string y diga si tiene más de 8 caracteres.
+//Suma de Dígitos: (Difícil) Toma el número 123 y suma $1 + 2 + 3 = 6$.
 fn main() {
-    let mut contador = 0;
-    let password = String::from("aredqaerqd2819ywqyg73");
-    for _i in password.chars() {
-        contador += 1;
+    let mut n = 123;
+    println!("la suma de los numeros {:?} es igual a ", n);
+    let mut suma = 0;
+    while n > 0 {
+        suma += n % 10;
+        n /= 10;
     }
-    println!("hay {:?} caracteres en la oracion {:?}", contador, password);
+    println!("{}", suma)
 }
