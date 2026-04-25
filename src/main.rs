de1ee373abf5f366d8611cee2d66c6919761ce0a
Iblameportacio <gm.portacio@gmail.com>
2026-04-24 19:12:40 -0500

    factorial de 10

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,8 @@
 //Verificador de Rango: Función que diga si un número está entre 10 y 20 (inclusive).
 fn main() {
-    let uno: i32 = 1;
-    let solucion: bool = esta(uno);
-    println!(
-        "el numero {:?} esta en el rango  entre 10 y 20? {:?}",
-        uno, solucion
-    );
-}
-fn esta(n: i32) -> bool {
-    if n >= 10 && n <= 20 {
-        return true;
-    } else {
-        return false;
+    let mut inicio = 1;
+    for i in 1..=10 {
+        inicio *= i;
     }
+    println!("el factorial de 10 es {:?}", inicio)
 }
