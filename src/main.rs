98b608203f691b694a7474ef4a7a9b84ecffb85c
Iblameportacio <gm.portacio@gmail.com>
2026-04-24 19:08:39 -0500

    ran 10 a 20

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
-//Fibonacci Simple: Imprime los primeros 10 números de la secuencia de Fibonacci.
-// es igual a 0+1+1+2+3+4+5+6+7+8+9+10= 1,2,3,5,8,13,21...
+//Verificador de Rango: Función que diga si un número está entre 10 y 20 (inclusive).
 fn main() {
-    let mut a = 0;
-    let mut b = 1;
-    for _i in 0..10 {
-        let i = a + b;
-        let solucion = a;
-        println!("{:?}", solucion);
-        a = b;
-        b = i;
+    let uno: i32 = 1;
+    let solucion: bool = esta(uno);
+    println!(
+        "el numero {:?} esta en el rango  entre 10 y 20? {:?}",
+        uno, solucion
+    );
+}
+fn esta(n: i32) -> bool {
+    if n >= 10 && n <= 20 {
+        return true;
+    } else {
+        return false;
     }
 }
