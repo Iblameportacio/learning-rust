541cb2349dd6d96b7e98af786e1c67c35d6e1dd8
Iblameportacio <gm.portacio@gmail.com>
2026-04-26 15:03:39 -0500

    MCD de dos numeros

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,27 @@
-//FizzBuzz Extreme: Del 1 al 100, múltiplos de 3 (Fizz), de 5 (Buzz), de ambos (FizzBuzz) y de 7 (Bazz).
+//Mínimo Común Múltiplo: Crea una función para hallarlo entre dos números.(el numero mas pequeño
+// que es multiplo de ambos)
 fn main() {
-    for i in 1..=100 {
-        if i % 7 == 0 && i % 5 == 0 && i % 3 == 0 {
-            println!("{} Bazz", i)
-        } else if i % 3 == 0 && i % 5 == 0 {
-            println!("{} FizzBuzz", i)
-        } else if i % 3 == 0 {
-            println!("{} Fizz", i)
-        } else if i % 5 == 0 {
-            println!("{} Buzz", i)
+    let a: i32 = 45;
+    let b: i32 = 18;
+    let solucion: i32 = mcm(a, b);
+    println!("el mcm de los numeros {:?} y {:?} es {:?}", a, b, solucion);
+}
+
+fn mcm(n: i32, m: i32) -> i32 {
+    let mut candidato;
+    if n > m {
+        candidato = n;
+    } else {
+        candidato = m;
+    }
+
+    loop {
+        if candidato % n == 0 && candidato % m == 0 {
+            return candidato;
+        } else {
+            candidato += 1;
         }
     }
 }
+//diferencia clave en un loop break sale del loop
+// return sale de la funcion
