9a8d19517fbcec8767068cc22e554ef3f32fc5b3
Iblameportacio <gm.portacio@gmail.com>
2026-04-26 17:47:47 -0500

    simulador de cajero

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,23 @@
-//Ordenamiento Simple: Dado un vector [3, 1, 4, 2], ordénalo de menor a mayor (puedes usar .sort()).
+//Simulador de Cajero: Tienes un saldo inicial, crea funciones para retirar y depositar.
 fn main() {
-    let mut numbers = vec![3, 1, 4, 2];
-    numbers.sort();
-    println!("the vector {:?} has been solved", numbers);
+    let saldo: f32 = 0.0;
+    let deposito: f32 = 40.0;
+    let retiro: f32 = 32.4;
+
+    println!("tu saldo actual es {:?}", saldo);
+
+    let saldo_despues_deposito = depositar(saldo, deposito);
+    let saldo_total = retirar(saldo_despues_deposito, retiro);
+
+    println!("depositaste {:?}", deposito);
+    println!("retiraste {:?}", retiro);
+    println!("tu saldo total disponible es {:?}", saldo_total);
+}
+
+fn depositar(n: f32, cantidad: f32) -> f32 {
+    n + cantidad
+}
+
+fn retirar(n: f32, cantidad: f32) -> f32 {
+    n - cantidad
 }
