4ef5cf96eb91fe9a78842a3902a6d0cc375421c2
Iblameportacio <gm.portacio@gmail.com>
2026-04-26 15:46:01 -0500

    is this word a palindrome?

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,10 @@
-//Mínimo Común Múltiplo: Crea una función para hallarlo entre dos números.(el numero mas pequeño
-// que es multiplo de ambos)
+//Palíndromo: Función que diga si una palabra se lee igual al revés (ej: "ana").
 fn main() {
-    let a: i32 = 45;
-    let b: i32 = 18;
-    let solucion: i32 = mcm(a, b);
-    println!("el mcm de los numeros {:?} y {:?} es {:?}", a, b, solucion);
+    let sentence = String::from("oso"); //we already know this is a palindrome
+    let solved: bool = palindrome(sentence.clone());
+    println!("the sentence {:?} is a palindrome? {:?}", sentence, solved);
 }
-
-fn mcm(n: i32, m: i32) -> i32 {
-    let mut candidato;
-    if n > m {
-        candidato = n;
-    } else {
-        candidato = m;
-    }
-
-    loop {
-        if candidato % n == 0 && candidato % m == 0 {
-            return candidato;
-        } else {
-            candidato += 1;
-        }
-    }
+fn palindrome(s: String) -> bool {
+    let z = s.chars().rev().collect::<String>();
+    z == s
 }
-//diferencia clave en un loop break sale del loop
-// return sale de la funcion
