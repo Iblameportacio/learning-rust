b8cbd53daad228ee5a148339b17c2cc20f503cda
Iblameportacio <gm.portacio@gmail.com>
2026-04-28 20:18:42 -0500

    level 2

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,3 @@
-//El Reloj: Un bucle que imprima "Minuto X, Segundo Y" hasta llegar a 5 minutos.
 fn main() {
-    let segundos = 0;
-    let minutos = 0;
-    for i in minutos..=5 {
-        for s in segundos..=59 {
-            println!("minuto {} segundo {}", i, s)
-        }
-    }
+    println!("level 2");
 }
